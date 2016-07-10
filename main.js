1f9e38d639aa85cd15346571d6fdfe8413bde56a
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-10 10:58:39 -0700

    .js on keydown a, playerOne move

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
 $(document).on('ready', function(){
 
-function $player(){
-  $( ".playerOne" ).click(function() {
-    $( ".playerOne" ).animate({ "left": "+=50px" }, "slow" );
-  });
+  function $player(){
+    $('body').keydown(function(e) {
 
-  $( ".playerTwo" ).click(function() {
-    $( ".playerTwo" ).animate({ "right": "+=50px" }, "slow" );
-  });
+        if(e.which =='65'){
+          $( ".playerOne" ).animate({ "left": "+=50px" }, "fast " );
+        }
+    });
 
-}
-$player();
+
+    $( ".playerTwo" ).click(function() {
+      $( ".playerTwo" ).animate({ "right": "+=50px" }, "slow" );
+    });
+
+
+
+  }
+  $player();
 
 
 
 
 
 }); // end $(document)
+
+// use .finish() and set that as a px value for the finish line
+//might also use the $fx.off method
+
+// backgrouns-size: covered / contain
+
+
+/*$("body").keydown(function(e) {
+    var offset = 5;
+    if(e.keyCode == '39') {
+        var pos = $("#d").position();
+        $("#d").css('left', pos.left + offset);
+*/
