b242fa35b403ea43d16a33f99bf258f076e43bf3
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-10 19:11:19 -0700

    make new .js, to make it more oop

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
 $(document).on('ready', function(){
 
+  function p1(){
+    $('body').keyup(function(e){
+      function movePOne(){
+        $( ".playerOne" ).animate({ "left": '+=100px' }, "fast" );
+      }
+      if(e.which =='65'){
+        movePTwo();
+      }
+    });
+p1();
+  function p2(){
+    $('body').keyup(function (e){
+      function movePTwo(){
+        $( ".playerTwo" ).animate({ "right": '+=100px' }, "fast" );
+      }
+
+      if(e.which =='76'){
+        movePTwo();
+      }
+
+    });
+  }
+p2();
+
+
+
+
+
+
+});
+/*
   function $player(){
     $('body').keyup(function(e) {
        var moveLength = "+=100px";//length of move per keyup
-       //var keyupCounterOne = 0;// counter for
+       //var counterOne=0;// counter for
        function movePOne(){
          $( ".playerOne" ).animate({ "left": moveLength }, "fast " );
        }
 
-        if(e.which =='65'){
-          movePOne();
-        }
-    });
+         if(i<3 && e.which =='65'){
+           movePOne();
+         }
+      }
+
+
+        //if(keyupCounterOne%2===0 && e.which =='65'){
+          //movePOne();
+        //} else if (keyupCounterOne%2!==0 && e.which =='83'){
+          //movePOne();
+        //}
+      //counterOne++;
+      });
 
 
   //  $( ".playerTwo" ).click(function() {
