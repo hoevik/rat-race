6a99d3745e69eac8f254236792cda9bb7d279192
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-10 20:56:50 -0700

    trying to make Player function in one round. yep, I know.. phony af

--- test.js
+++ test.js
@@ -1,15 +1,21 @@
 console.log('sanity check');
 $(document).on('ready', function(){
-var p1 = new player('test one', 'left', 65);
-var p2 = new player('test two', 'right' 76);
+  var p1 = new Player('.playerOne', 'left', '65');
+  var p2 = new Player('.playerTwo', 'right', '76');
 
-function player(name, direction, key){
+  function Player(className, direction, key){
+    this.name= 'className';
+    this.dir='dir';
+    this.key='key';
+    //this.$racer = $("." + racerid); // this.$racer to indicate jQuery
+    this.move = function(){
+      $(this.name).css(this.dir,("+=25px"));
+  };
 
-}
-
-}); // end $(document)
+  }
 
 
+  }); // end $(document)
 /*
 $('body').keyup(function game(q){
   var speed = '+=100px';
