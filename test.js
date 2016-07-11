54ff9977d806847936e45daf7ee527441b484c58
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-10 20:20:08 -0700

    player function upand running. probably

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 console.log('sanity check');
 $(document).on('ready', function(){
+var p1 = new player('test one', 'left', 65);
+var p2 = new player('test two', 'right' 76);
+
+function player(name, direction, key){
+
+}
+
+}); // end $(document)
 
 
+/*
+$('body').keyup(function game(q){
+  var speed = '+=100px';
 
+  function p1(){
 
-});
+  }
+}); // end game function
+
+*/
