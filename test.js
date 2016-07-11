65546a6103fa06b20b1c15e9ff85255701995fa8
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-10 22:12:48 -0700

    give up on test oop for now, ask for help

--- test.js
+++ test.js
@@ -1,19 +1,30 @@
 console.log('sanity check');
 $(document).on('ready', function(){
-  var p1 = new Player('.playerOne', 'left', '65');
-  var p2 = new Player('.playerTwo', 'right', '76');
 
   function Player(className, direction, key){
     this.name= 'className';
-    this.dir='dir';
+    this.dir= 'direction';
     this.key='key';
-    //this.$racer = $("." + racerid); // this.$racer to indicate jQuery
-    this.move = function(){
-      $(this.name).css(this.dir,("+=25px"));
-  };
 
+    this.move = function (){
+      $('name').css(dir,("+=25px"));
+    }; // end f this.move
+  }//end f Player
+
+
+  var p1 = new Player('.playerOne', 'left', '65');
+  var p2 = new Player('.playerTwo', 'right', '76');
+  var move;
+
+  function movePlayers(p1, p2){
+    $('body').keyup(function (e){
+      if ((e.which==p1.key)){
+          p1.move();
+      }
+
+    });
   }
-
+movePlayers();
 
   }); // end $(document)
 /*
