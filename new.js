cdb598e73bd001e13a1c046219adbc284f8af6e6
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-11 06:59:05 -0700

    rename game, and try to get button to reset game, but ney. not this time

--- new.js
+++ new.js
@@ -1,23 +1,32 @@
 $(document).on('ready', function(){
 
-function p1(){
-  $('body').keyup(function(e){
-    if(e.which =='65'){
-      $('.playerOne').animate({ "left": '+=100px' }, "fast" );
-    }
-  });
-}
+  function p1(){
+    $('body').keyup(function(e){
+      if(e.which =='65'){
+        $('.playerOne').css("left", ('+=25px'));
+      }
+    });
+  }
 
 
-p1();
-function p2(){
-  $('body').keyup(function(e){
-    if(e.which =='76'){
-      $('.playerTwo').animate({ "right": '+=100px' }, "fast" );
-    }
-  });
-}
-p2();
+
+
+  p1();
+  function p2(){
+    $('body').keyup(function(e){
+      if(e.which =='76'){
+        $('.playerTwo').css("right", ('+=25px'));
+      }
+    });
+  }
+  p2();
+
+  $('button').on("click", function(e){ // reset button works by removing classes, values, and text
+      $(".playerOne").css("left ",("0"));
+
+    });
+
+
 
 
 });
