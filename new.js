ea4fcb96ff839504f821882ccd329f45fd731c75
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-11 07:13:45 -0700

    found a lazy way to alert winner... yey

--- new.js
+++ new.js
@@ -1,17 +1,20 @@
 $(document).on('ready', function(){
 
   function p1(){
+    var c1=0
     $('body').keyup(function(e){
       if(e.which =='65'){
         $('.playerOne').css("left", ('+=25px'));
       }
+      c1++;
+      if(c1>3){
+        alert('fucker')
+      }
     });
+
   }
-
-
-
-
   p1();
+
   function p2(){
     $('body').keyup(function(e){
       if(e.which =='76'){
@@ -20,6 +23,9 @@ $(document).on('ready', function(){
     });
   }
   p2();
+
+
+
 
   $('button').on("click", function(e){ // reset button works by removing classes, values, and text
       $(".playerOne").css("left ",("0"));
