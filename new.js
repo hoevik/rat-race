35ebbbcc7a6740c0ae47a0c4d20797d4867cf13c
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-12 09:40:06 -0700

    reset and saluting a winner without alert now works

--- new.js
+++ new.js
@@ -1,51 +1,49 @@
 $(document).on('ready', function(){
-
-  function p1(){
-    var count1=0;
-    $('body').keyup(function(e){
-      if(e.which =='65'){
-        $('.playerOne').css("left", ('+=25px'));
-        count1++;
-      }
-       if(count1>22){
-        $('.winner').html('<h2>Player 1 wins<h2>');
-       }
-    });
-  }
+  function p1() {
 
 
-  function p2(){
-    var count2=0;
-    $('body').keyup(function(e){
-      if(e.which =='76'){
-        $('.playerTwo').css("right", ('+=25px'));
-        count2++;
-      }
-      if(count2>22){
-        //alert('A winner has no name. Thoug the loosers name is P1');
-        $('.winner').html('<h2>Player 2 wins<h2>');
-      }
-    });
-  }
+      var count1=0;
+      var score1=0;
+      $('body').keyup(function(e){
+        if(e.which =='65'){
+          $('.playerOne').css({left:'+=25'});
+          count1++;
+        }
+         if(count1===22){
+          $('.winner').html('<h2>Player 1 wins<h2>');
+          score1++;
+          $('body').off("keyup");
+         }
+      });
+    }
 
-function stop(){
-  if (key)$('body').off('keyup');
-}
 
-  function $restart(){
-    $('button').on('click', function(){
-      p1();
-      p2();
-      stop();
-      $('.playerOne').css({'left':'0'});
-      $('.playerTwo').css({'right':'0'});
-    });
-  }
+    function p2() {
+      var count2=0;
+      var score2=0;
+      $('body').keyup(function(e){
+        if(e.which =='76'){
+          $('.playerTwo').css({right:'+=25'});
+          count2++;
+        }
+        if(count2===22){
+          $('.winner').html('<h2>Player 2 wins<h2>');
+          score2++;
+          $('body').off("keyup");
+        }
+      });
+    }
 
-  p1();
-  p2();
-
+      $('button').on('click', function(){
+        p1();
+        p2();
+        $('.playerOne').css({'left':'0'});
+        $('.playerTwo').css({'right':'0'});
+        $('.winner').html('');
+      });
+p1();
+p2();
 
 
 
-});
+  });
